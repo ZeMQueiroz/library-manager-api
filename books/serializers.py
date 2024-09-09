cea4953d9da095ee5beb49bef3a28215ee99af3a
Jose Queiroz <zemqueiroz@gmail.com>
2024-09-09 23:11:45 +0100

    serializer fix

--- books/serializers.py
+++ books/serializers.py
@@ -1,4 +1,3 @@
-# books/serializers.py
 from rest_framework import serializers
 from .models import MediaItem, Category, CustomList
 
@@ -11,12 +10,16 @@ class CategorySerializer(serializers.ModelSerializer):
         }
 
 class MediaItemSerializer(serializers.ModelSerializer):
+    # Explicitly define the category field to use PrimaryKeyRelatedField
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+
     class Meta:
         model = MediaItem
         fields = '__all__'
 
 class CustomListSerializer(serializers.ModelSerializer):
     items = MediaItemSerializer(many=True, read_only=True)
+    # Ensure category field is correctly configured in CustomListSerializer as well
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
 
     class Meta:
